b2b0b6665550b4a3bbdef59e06cad0bdfed61904
Evan Boggs Jr <evanboggs@Evans-MacBook-Pro.local>
2019-07-01 16:58:04 -0700

    getting there

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { Switch, Route, NavLink } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Header/Header";
+import AboutMe from "./Components/About/AboutMe";
+// import Conact from "./Components/About/Contact/Contact";
+// import Projects from "./Components/Projects/Projects";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={} />
-        {/* <Route path='/' component={}/> */}
-        {/* <Route path='' component={}/> */}
-        {/* <Route path='' component={}/> */}
-      </Switch>
+      {/* <Header /> */}
+      {/* <Switch> */}
+      {/* <Route exact path="/" component={AboutMe} /> */}
+      {/* <Route path='/contact' component={Contact}/> */}
+      {/* <Route path='/projects' component={Projects}/> */}
+      {/* <Route path='' component={}/> */}
+      {/* </Switch> */}
     </div>
   );
 }
